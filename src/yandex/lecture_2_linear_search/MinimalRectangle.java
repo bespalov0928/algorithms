536b91e259b64e7c60b13814d23f6f0c50bf5726
nvk <bespalov0928@mail.ru>
2024-04-20 22:38:42 +0300

    Задача: линейный поиск. A. Минимальный прямоугольник.

---
+++ src/yandex/lecture_2_linear_search/MinimalRectangle.java
@@ -0,0 +1,53 @@
+package yandex.lecture_2_linear_search;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MinimalRectangle {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer k = Integer.valueOf(br.readLine());
+        String[] str = br.readLine().split(" ");
+        int minx = Integer.valueOf(str[0]);
+        int miny = Integer.valueOf(str[1]);
+        int maxx = minx;
+        int maxy = miny;
+
+        for (int index = 2; index <= k; index++) {
+            String[] str_ = br.readLine().split(" ");
+            int x = Integer.valueOf(str_[0]);
+            int y = Integer.valueOf(str_[1]);
+
+            minx = Math.min(minx, x);
+            miny = Math.min(miny, y);
+            maxx = Math.max(maxx, x);
+            maxy = Math.max(maxy, y);
+
+        }
+        System.out.println(String.format("%s %s %s %s", minx, miny, maxx, maxy));
+    }
+
+    public String Start(String[] arr) {
+        Integer k = Integer.valueOf(arr[0]);
+        String[] str = arr[1].split(" ");
+        int minx = Integer.valueOf(str[0]);
+        int miny = Integer.valueOf(str[1]);
+        int maxx = minx;
+        int maxy = miny;
+
+        for (int index = 2; index <= k; index++) {
+            String[] str_ = arr[index].split(" ");
+            int x = Integer.valueOf(str_[0]);
+            int y = Integer.valueOf(str_[1]);
+
+            minx = Math.min(minx, x);
+            miny = Math.min(miny, y);
+            maxx = Math.max(maxx, x);
+            maxy = Math.max(maxy, y);
+
+        }
+        return String.format("%s %s %s %s", minx, miny, maxx, maxy);
+    }
+
+}
---
+++ src/yandex/lecture_2_linear_search/MinimalRectangleTest.java
@@ -0,0 +1,19 @@
+package yandex.lecture_2_linear_search;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinimalRectangleTest {
+
+    @Test
+    void start() {
+        MinimalRectangle minimalRectangle = new MinimalRectangle();
+        String[] arr = new String[]{"4", "1 3", "3 1", "3 5", "6 3"};
+        String rsl = minimalRectangle.Start(arr);
+        System.out.println(rsl);
+        Assertions.assertEquals(rsl, "1 1 6 5");
+
+    }
+}
